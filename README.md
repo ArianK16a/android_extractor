118409ef345740b037eeb8af6cc935745566ae12
Arian <arian.kulmer@web.de>
2022-02-26 11:43:12 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+### First usage ###
+```
+git submodule update --recursive
+./apply_patch.sh
+```
+
+### How to use ###
+```
+./extract_rom.py </path/to/rom.zip> [out/dir]
+```
--- apply_patch.sh
+++ apply_patch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd utils/extract_android_ota_payload/
 git am ../../patches/extract_android_ota_payload/0001-Update-include-for-use-as-utils-module.patch
 cd ../..
